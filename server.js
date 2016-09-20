89c5bdbea723a1e8ad5f31edb50e05f08b5ba25e
Zivile <zivilytez@hotmail.com>
2016-09-20 03:49:52 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -3,9 +3,34 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-mongoose.connect('mongodb://heroku_fnjw7508:3jlnk9k1nm9a83jfa3h1lv70c0@ds035786.mlab.com:35786/heroku_fnjw7508');
+//mongoose.connect('mongodb://heroku_fnjw7508:3jlnk9k1nm9a83jfa3h1lv70c0@ds035786.mlab.com:35786/heroku_fnjw7508');
 
 var app = express();
+
+var db;
+
+app.all('/*', function(req, res) {
+  res.send('hello world');
+});
+
+mongoose.connect(process.env.MONGODB_URI, function(err, database) {
+    if (err) {
+      console.log(err);
+     process.exit(1);
+   }
+   db = database;
+   console.log('Database connection ready');
+ 
+   var server = app.listen(process.env.PORT || 8080, function() {
+     var port = server.address().port;
+     console.log('App now running on port ', port);
+   });
+ });
+ 
+ function handleError(res, reason, message, code) {
+   console.log('ERROR: ' + reason);
+    res.status(code || 500).json({'error': message});
+  }
 
 app.use(express.static(__dirname + '/public')); 
 
@@ -24,11 +49,11 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 // Routes
 app.use('/api', require('./routes/api'));
 
-var port = process.env.PORT || 8080;
+// var port = process.env.PORT || 8080;
 
-app.listen(port);               
+// app.listen(port);               
            
-console.log('Magic happens on port ' + port);
+// console.log('Magic happens on port ' + port);
    
 exports = module.exports = app;                         
 
