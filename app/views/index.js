32567a098d78d0f1d88253a44188b51ddaf8912f
Zivile <zivilytez@hotmail.com>
2016-09-20 11:03:28 -0500

    still working with server, added first index view page

---
+++ app/views/index.js
@@ -0,0 +1,12 @@
+</html></!DOCTYPE html>
+<html>
+<head>
+  <title>Shop Bubble App</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Some start</h1>
+  </div>
+</body>
+</html>
--- server.js
+++ server.js
@@ -12,10 +12,14 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+app.get('/', function(req, res) {
 
-app.all('/*', function(req, res) {
-  res.send('hello world');
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
+// app.all('/*', function(req, res) {
+//   res.send('hello world');
+// });
 
 // Connect to the database before starting the application server.
 mongoose.connect(process.env.MONGODB_URI, function (err, database) {
