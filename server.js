ff013d72b74583c7a14a13cefbfb3fabccf2580b
Zivile <zivilytez@hotmail.com>
2016-09-20 03:53:28 -0500

    changing server.js to original one

--- server.js
+++ server.js
@@ -1,59 +1,35 @@
-//modules
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-//mongoose.connect('mongodb://heroku_fnjw7508:3jlnk9k1nm9a83jfa3h1lv70c0@ds035786.mlab.com:35786/heroku_fnjw7508');
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-
 app.all('/*', function(req, res) {
   res.send('hello world');
 });
 
-mongoose.connect(process.env.MONGODB_URI, function(err, database) {
-    if (err) {
-      console.log(err);
-     process.exit(1);
-   }
-   db = database;
-   console.log('Database connection ready');
- 
-   var server = app.listen(process.env.PORT || 8080, function() {
-     var port = server.address().port;
-     console.log('App now running on port ', port);
-   });
- });
- 
- function handleError(res, reason, message, code) {
-   console.log('ERROR: ' + reason);
-    res.status(code || 500).json({'error': message});
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
   }
 
-app.use(express.static(__dirname + '/public')); 
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-// parse application/json 
-app.use(bodyParser.json()); 
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
-
-// Routes
-app.use('/api', require('./routes/api'));
-
-// var port = process.env.PORT || 8080;
-
-// app.listen(port);               
-           
-// console.log('Magic happens on port ' + port);
-   
-exports = module.exports = app;                         
-
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
