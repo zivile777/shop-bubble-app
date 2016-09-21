6f50a57478f838433843a0dd0f516f04d60064a3
Zivile <zivilytez@hotmail.com>
2016-09-21 08:58:56 -0500

    config back to mongodb

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url : 'mongodb://heroku_6txrsrqj:2is39mgugt4qmooh5h6jdc6lk6@ds035816.mlab.com:35816/heroku_6txrsrqj'
+  //url : 'mongodb://heroku_6txrsrqj:2is39mgugt4qmooh5h6jdc6lk6@ds035816.mlab.com:35816/heroku_6txrsrqj'
+  url : 'mongodb://localhost/'
 };
