d6480f34d4dcae54905bdd98fac74a00474b0e1b
Zivile <zivilytez@hotmail.com>
2016-09-21 16:00:49 -0500

    update on db

--- server.js
+++ server.js
@@ -4,10 +4,25 @@
   var morgan   = require('morgan');
   var bodyParser = require('body-parser');
   var methodOverride = require('method-override');
-  var database = require('./config/database');
-  var port     = process.env.PORT || 8888;
-
-  mongoose.connect(process.env.MONGOLAB_URI || database.url);
+  //var database = require('./config/database');
+  //var port     = process.env.PORT || 8888;
+  var db;
+  var mongoUri = process.env.MONGODB_URI;
+  mongodb.MongoClient.connect(mongoUri, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready!");
+  // Initialize the app.
+  var server = app.listen(port, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+//mongoose.connect(process.env.MONGOLAB_URI || database.url);
 
   app.use(express.static(__dirname + '/public')); 
   app.use(morgan('dev'));
@@ -18,7 +33,7 @@
 
   require('./app/routes.js')(app);
 
-  app.listen(port);
-  console.log("App listening on port : " + port);
+  // app.listen(port);
+  // console.log("App listening on port : " + port);
 
   module.exports = app;
